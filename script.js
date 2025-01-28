85b87cfbf379b05568b8e6c77f7e57a0c85c00b9
Munira Yusuf Ali Attar <124035031+Muniraattar3@users.noreply.github.com>
2025-01-29 00:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent the default form submission behavior
+    event.preventDefault();
 
+    // Access the form element
+    const form = document.getElementById('form1');
+
+    // Get the values of the input fields using the `name` attribute
+    const firstName = form.elements['fname'].value;
+    const lastName = form.elements['lname'].value;
+
+    // Display the values in an alert
+    alert(`${firstName} ${lastName}`);
 }
